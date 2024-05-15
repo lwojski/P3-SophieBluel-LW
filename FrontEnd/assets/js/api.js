4ef53ba4cfed63e8a899b4dc01943f37f6ee940d
lwojski <lilian.woj@icloud.com>
2024-05-15 19:58:21 +0200

    Formulaire de connexion fonctionnel

--- FrontEnd/assets/js/api.js
+++ FrontEnd/assets/js/api.js
@@ -2,14 +2,49 @@
 
 const url='http://localhost:5678/api/'
 
-// CATÉGORIES
+// (CATÉGORIES)
 export async function getCategories() {
     return fetch(`${url}categories`)
         .then(response => response.json())
 }
 
-// WORKS
+// (WORKS)
 export async function getWorks() {
     return fetch(`${url}works`)
         .then(response => response.json())
+}
+
+// LOGIN
+
+export async function login () {
+
+    document.addEventListener('submit', (event) => {
+        event.preventDefault()
+        let formInfo = {
+            email: document.getElementById('email'),
+            password: document.getElementById('password'),
+            submit: document.querySelector('.submitInfo')
+        }
+    
+        return fetch(`${url}users/login`, {
+            method: "POST",
+            headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                email: formInfo.email.value,
+                password: formInfo.password.value,
+            }),
+        })
+        .then(response => response.json())
+        .then((data) => {
+            if (data.message) {
+                alert("Erreur dans l’identifiant ou le mot de passe")
+            } else {
+                sessionStorage.setItem("token", data.token)
+                window.location.replace("index.html")
+            }
+        })
+    })
 }
---
+++ FrontEnd/login.js
@@ -0,0 +1,3 @@
+import {login} from "./assets/js/api.js"
+
+const loginUser = await login()
